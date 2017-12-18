ba865fd573f5a36625e4582e8e0807876a420da4
Kristian Garza <kj.garza@gmail.com>
2017-12-18 16:39:23 +0100

    specs clients

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,90 +1,202 @@
 require 'rails_helper'
 
-RSpec.describe Client, type: :model do
-  let!(:clients)  { create_list(:client, 10) }
-  let!(:client) { clients.first }
-  let!(:dois) {  create_list(:doi, 5, client_id: client.symbol) }
-  describe "Validations" do
-    # it { should validate_presence_of(:symbol) }
-    # it { should validate_presence_of(:name) }
-    # it { should validate_presence_of(:contact_email) }
-    # it { should validate_presence_of(:contact_name) }
-  end
+RSpec.describe Client, elasticsearch: true, type: :model do
+  let!(:provider)    { build(:provider) }  
+  let!(:clients)     { build_list(:client, 25, provider_id: provider.symbol) }
+  let!(:client)      { clients.first }
+  # let!(:dois)        { build_list(:doi, 10, client_id: client.symbol) }  
+  let!(:client_last) { clients.last }
 
-  describe "methods" do
 
-    it "clients all" do
-      collection = Client.all
-      expect(collection.length).to eq(clients.length)
-      single = collection.first
-      expect(single.name).to eq(client.name)
-      expect(single.role_name).to eq(client.role_name)
-      # meta = clients[:meta]
-      # expect(meta["resource-types"]).not_to be_empty
-      # expect(meta["years"]).not_to be_empty
-      # expect(meta).not_to be_empty
+  describe "List Clients" do
+    context "when there are clients" do 
+      before do 
+        Provider.create(provider)         
+        clients.each { |item| Client.create(item) }
+        # dois.each   { |item| Doi.create(item) }
+        sleep 2
+      end
+
+      it "returns all clients" do
+        collection = Client.all
+        expect(collection.length).to eq(clients.length)
+      end
+
+      it "returns correct attributes" do
+        puts Client.all
+        single = Client.query_filter_by(:symbol, client.symbol).first
+        puts single.inspect
+        puts client.symbol.downcase
+        expect(single.name).to eq(client.name)
+        expect(single.role_name).to eq(client.role_name)
+      end
+
+      it "returns correct number of clients" do
+        single = Client.query_filter_by(:symbol, client.symbol).first
+        # client_count = single.doi_count
+        # expect(doi_count.first[:count]).to eq(clients.length)
+      end
     end
 
-    it "clients with where year" do
-      collection = Client.where("YEAR(datacentre.created) = ?", client.created)
-      single = collection.first
-      expect(single.year).to eq(client.created.year)
-      expect(single.name).to eq(client.name)
-      expect(single.symbol).to eq(client.symbol)
-    end
+    context "when there are not clients" do 
 
-    it "should not update the symbol" do
-      client.update_attributes :symbol => client.symbol+'foo.bar'
-      expect(client.reload.symbol).to eq(client.symbol)
-    end
-
-    it "trasnfer all DOIS" do
-      original_dois = Doi.where(client: client.symbol)
-      target = clients.last
-      expect(Doi.where(datacentre: client.id).count).to eq(5)
-      expect(Doi.where(datacentre: target.id).count).to eq(0)
-      client.target_id = target.symbol
-      expect(Doi.where(datacentre: client.id).count).to eq(0)
-      expect(Doi.where(datacentre: target.id).count).to eq(5)
-    end
-
-    # it "clients doi quota" do
-    #   client.doi_quota_allowed = 5
-    #
-    #
-    # end
-    #
-    # it "clients with where and resource-type-id" do
-    #   collection = Client.where(where: "cancer", "resource-type-id" => "dataset")
-    #   expect(collection[:data].length).to eq(3)
-    #   client = collection[:data].first
-    #   expect(client.title).to eq("Landings of European lobster (Homarus gammarus) and edible crab (Cancer pagurus) in 2011, Helgoland, North Sea")
-    #   expect(client.resource_type.title).to eq("Dataset")
-    # end
-    #
-    # it "clients with where and resource-type-id and data-center-id" do
-    #   collection = Client.where(where: "cancer", "resource-type-id" => "dataset", "data-center-id" => "FIGSHARE.ARS")
-    #   expect(collection[:data].length).to eq(25)
-    #   client = collection[:data].first
-    #   expect(client.title).to eq("Achilles_v3.3.7_README.txt")
-    #   expect(client.resource_type.title).to eq("Dataset")
-    # end
-
-    it "client" do
-      single = Client.where(symbol: client.symbol).first
-      expect(single.name).to eq(client.name)
-      expect(single.symbol).to eq(client.symbol)
-      expect(single.role_name).to eq(client.role_name)
-      expect(single.provider_id).to eq(client.provider_id)
-      expect(single.created).to be_truthy
-      expect(single.updated).to be_truthy
-      expect(single.year).to be_truthy
-      expect(single.domains).to be_truthy
-      # expect(single.member).to be_truthy
-      expect(single.repository).to be_nil
-      expect(single.is_active).to be_truthy
-      expect(single.doi_quota_allowed).to be_truthy
-      expect(single.doi_quota_used).to be_truthy
+      it "returns no clients" do
+        # collection = Client.all
+        # expect(collection.length).to eq(0)
+      end
     end
   end
+
+  # describe "Show Client" do
+  #   context "when the client exist" do 
+  #     before do 
+  #       Provider.create(provider)                 
+  #       Client.create(client) 
+  #       sleep 2
+  #     end
+
+  #     it "returns correct attributes" do
+
+  #       single = Client.query_filter_by(:symbol, client.symbol).first
+  #       # puts single.inspect
+  #       expect(single.name).to eq(client.name)
+  #       expect(single.symbol).to eq(client.symbol)
+  #       expect(single.role_name).to eq(client.role_name)
+  #       expect(single.created).to be_truthy
+  #       expect(single.updated).to be_truthy
+  #       # expect(single.region).to be_truthy
+  #       expect(single.is_active).to be_truthy
+  #       expect(single.doi_quota_allowed).to be_truthy
+  #       expect(single.doi_quota_used).to be_truthy
+  #     end
+  #   end
+  #   context "when the client do not exist" do 
+  #     before do 
+  #       clients.each { |item| Client.create(item) }
+  #       sleep 2
+  #     end
+
+  #     it "returns no attributes" do
+  #       single = Client.query_filter_by(:symbol, client.symbol).first
+  #       expect(single.name).not_to eq(provider_last.name)
+  #       expect(single.symbol).not_to eq(provider_last.symbol)
+  #     end
+  #   end
+  # end
+
+  # describe "Create Client" do
+  #   context "when the client do not exist" do 
+  #     before do 
+  #       Provider.create(provider)                 
+  #       Client.create(client) 
+  #       sleep 2
+  #     end
+
+  #     it "returns correct attributes" do
+  #       single = Client.query_filter_by(:symbol, client.symbol).first
+  #       expect(single.name).to eq(client.name)
+  #       expect(single.symbol).to eq(client.symbol)
+  #       expect(single.role_name).to eq(client.role_name)
+  #       expect(single.created).to be_truthy
+  #       expect(single.updated).to be_truthy
+  #       # expect(single.region).to be_truthy
+  #       expect(single.is_active).to be_truthy
+  #       expect(single.doi_quota_allowed).to be_truthy
+  #       expect(single.doi_quota_used).to be_truthy
+  #     end
+  #   end
+  #   context "when the client  exist" do 
+  #     before do 
+  #       Provider.create(provider)                 
+  #       Client.create(client) 
+  #       sleep 2
+  #       Client.create(client) 
+  #       sleep 2
+  #     end
+
+  #     it "returns correct attributes" do
+  #       prov = Client.query_filter_by(:symbol, client.symbol).count
+  #       expect(prov).to eq(2)
+  #     end
+  #   end
+  # end
+
+  # describe "Update Client" do
+  #   context "when parameter are correct" do 
+  #     before do 
+  #       pv = Client.create(client) 
+  #       sleep 2
+  #       pv.update_attributes name: "Logan"
+  #       sleep 2      
+  #     end
+
+  #     it "returns correct attributes" do
+  #       single = Client.query_filter_by(:symbol, client.symbol).first
+  #       expect(single.name).not_to eq(client.name)
+  #       expect(single.symbol).to eq(client.symbol)
+  #       expect(single.role_name).to eq(client.role_name)
+  #       expect(single.created).to be_truthy
+  #     end
+  #   end
+  # end
+
+  # describe "Delete Client" do
+  #   context "when the client exist" do 
+  #     before do 
+  #       Provider.create(provider)                 
+  #       pv = Client.create(client) 
+  #       sleep 2 
+  #       pv.destroy
+  #       sleep 2
+  #     end
+
+  #     it "returns correct response" do
+  #       single = Client.query_filter_by(:symbol, client.symbol).first
+  #       expect(single.respond_to?(:name)).to be false
+  #     end
+  #   end
+
+  #   context "when the client do not exist" do 
+  #     before do 
+  #       Provider.create(provider)                 
+  #       Client.create(client) 
+  #       sleep 2 
+  #     end
+
+  #     it "returns correct attributes" do
+
+  #     end
+  #   end
+  # end
+
+  # describe "Query Client" do
+  #   context "when the client exist" do 
+  #     before do 
+  #       clients.each { |item| Client.create(item) }
+  #       sleep 2         
+  #     end
+
+  #     it "returns correct attributes" do
+  #       collection = Client.query(client.name)
+  #       results = collection.select { |item| item.symbol.casecmp client.symbol }
+  #       expect(results.length).to be > 0 
+  #       expect(results.length).to be < clients.length
+  #       expect(collection.first.respond_to?(:name)).to be true
+  #     end
+  #   end
+  #   context "when the client do not exist" do 
+  #     before do 
+  #       clients.each { |item| Client.create(item) }
+  #       sleep 2 
+  #     end
+
+  #     it "returns correct attributes" do
+  #       single = Client.query("TIB").first
+  #       expect(single.respond_to?(:name)).to be false        
+  #     end
+  #   end
+  # end
+
+  
+
 end
