a84f39653d5dbb4b61187d2d3ac6c8b0cdc976fb
Kristian Garza <kj.garza@gmail.com>
2018-02-11 18:57:30 +0100

    change api name to app

--- app/controllers/concerns/delegatable.rb
+++ app/controllers/concerns/delegatable.rb
@@ -7,15 +7,15 @@ require 'json'
     def dois_count uid, **options
       Rails.cache.fetch("dois_count/#{uid}", expires_in: 6.hours, force: options[:force]) do
         if self.is_a?(ClientsController)
-          response = Maremma.get(ENV['API_URL']+"clients/"+uid)  
+          response = Maremma.get(ENV['APP_URL']+"clients/"+uid)  
         elsif  self.is_a?(ProvidersController) 
-          response = Maremma.get(ENV['API_URL']+"providers/"+uid)  
+          response = Maremma.get(ENV['APP_URL']+"providers/"+uid)  
         end
         response.status == 200 ? response.body["meta"]["dois"] : "null"
       end
     end
 
-
+ 
     # def prefixes_count uid
     #   if self.is_a?(ClientsController)
     #     response = Maremma.get(ENV['API_URL']+"clients/"+uid)  
