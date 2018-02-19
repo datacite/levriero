fce02de71e9d679598770abebd20504539f1b95b
Martin Fenner <mfenner@datacite.org>
2018-02-19 23:57:41 +0100

    clean up controllers

--- app/controllers/concerns/facetable.rb
+++ app/controllers/concerns/facetable.rb
@@ -2,100 +2,20 @@ module Facetable
   extend ActiveSupport::Concern
 
   included do
-
-    def filter_by_client client_id, collection
-        collection = collection.respond_to?(:search) ? collection.query_filter_by(:client_id, client_id) : collection.select {|item| item.client_id == client_id}
-    end
-
-    def filter_by_provider provider_id, collection
-      collection = collection.respond_to?(:search) ? collection.query_filter_by(:provider_id, provider_id) : collection.select {|item| item.provider_id == provider_id}
-    end
-
-    def filter_by_publication published, collection
-      collection = collection.respond_to?(:search) ? collection.query_filter_by(:published, published) : collection.select {|item| item.published == published}
-    end
-
-    def filter_by_symbol symbol, collection
-        collection =collection.select {|item| item.symbol == symbol} unless collection.respond_to?(:search)
-        collection = collection.query_filter_by :symbol, symbol if collection.respond_to?(:search)
-    end
-
-    def filter_by_query query, collection
-      collection.query(query)
-    end
-
-    def filter_by_year year, collection
-      collection = collection.respond_to?(:search) ? collection.query_filter_by(:year, year) : collection.select {|item| item.year == year}
-    end
-
-    def filter_by_region region, collection
-      collection = collection.respond_to?(:search) ? collection.query_filter_by(:region, region) : collection.select {|item| item.region == region}
-    end
-
-
-    def filter_by_ids ids, collection
-      r = []
-      ids = ids.split(",")
-      if collection.respond_to?(:search)
-        # ids.each { |id|  r << Client.query_filter_by(:symbol, id).first }
-        ids.each { |id|  r << Client.find_by_id(id) if Client.find_by_id(id).present? }
-      else
-        ids.each { |id|  r << collection.select {|item| item.symbol == item}.first }
-      end
-      enumerator = r.to_enum(:each)
-      enumerator.each do |record| 
-        puts record
-        puts record.class.name
-      end
-      # enumerator.ddd
-      enumerator
-      # r
-    end
-
-    def filter_by_prefix prefix, collection
-      collection = collection.respond_to?(:search) ? collection.query_prefixes(prefix) : collection.select {|item| item.prefixes.include? prefix}
-    end
-
-    def facet_by_region params, collection
-      if params[:region].present?
-        regions = [{ id: params[:region],
-                     title: REGIONS[params[:region].upcase],
-                     count: collection.group_by{|record| record[:region]}.map{ |k,v| { id: k.to_s, title: k.to_s, count: v.count } }}]
-      else
-        # regions = collection.where.not(region: nil).group(:region).count
-        regions = collection.group_by{|record| record[:region]}
-        regions = regions.map { |k,v| { id: k.downcase, title: REGIONS[k], count: v.count } }
+    def facet_by_year(arr)
+      arr.map do |hsh|
+        { "id" => hsh["key_as_string"][0..3],
+          "title" => hsh["key_as_string"][0..3],
+          "count" => hsh["doc_count"] }
       end
     end
 
-    def facet_by_year params, collection
-      if params[:year].present?
-        years = collection.group_by{|record| record[:year]}.map{ |k,v| { id: k.to_s, title: k.to_s, count: v.count }}
-      else
-        # years = collection.where.not(created: nil).order("YEAR(allocator.created) DESC").group("YEAR(allocator.created)").count
-        years = collection.group_by{|record| record.year}
-        years = years.map { |k,v| { id: k.to_s, title: k.to_s, count: v.count } }
+    def facet_by_provider(arr)
+      arr.map do |hsh|
+        { "id" => hsh["key"],
+          "title" => hsh["key"],
+          "count" => hsh["doc_count"] }
       end
-      years.sort_by { |hsh| hsh[:id] }
     end
-
-    def get_providers collection, **options
-      # Rails.cache.fetch("providers_set", expires_in: 6.hours, force: options[:force]) do
-        providers = collection.group_by{|record| record[:provider_id]}
-        providers = providers.map { |k,v| { id: k.to_s.upcase, title: k.to_s, count: v.count } }
-        providers
-      # end
-     end
-    end
-
-    def filter_providers_by_client client_id, collection, **options
-      Rails.cache.fetch("providers_by_client/#{client_id}", expires_in: 6.hours, force: options[:force]) do
-        client =  Client.query_filter_by(:symbol, client_id)
-        if collection = collection.respond_to?(:search) 
-          collection = Provider.query_filter_by(:symbol, client.first[:provider_id])        
-        else
-          collection = collection.select {|provider| provider.symbol == client.provider_id}
-        end
-      end
-     end
+  end
 end
--- app/controllers/data_centers_controller.rb
+++ app/controllers/data_centers_controller.rb
@@ -3,48 +3,39 @@ class DataCentersController < ApplicationController
   before_action :set_include
 
   def index
- 
-    collection = Client
-    collection = filter_by_query params[:query], collection if params[:query].present?
+    if params[:id].present?
+      response = Client.find(params[:id])
+    elsif params[:ids].present?
+      response = Client.query(params[:ids])
+    else
+      page = (params.dig(:page, :number) || 1).to_i
+      size = (params.dig(:page, :size) || 25).to_i
+      from = (page - 1) * size
 
-    collection = filter_by_symbol params[:id], collection if params[:id].present?
-    collection = filter_by_provider params[:provider_id], collection if params[:provider_id].present?
-    collection = filter_by_prefix params[:prefix], collection if params[:prefix].present?
-    collection = filter_by_year params[:year], collection if params[:year].present?
- 
-    collection = filter_by_ids params[:ids], collection if params[:ids].present?
- 
-    collection = Client.all if collection.respond_to?(:search)
-    # regions    = facet_by_region params, collection
-    years      = facet_by_year params, collection
+      sort = case params[:sort]
+             when "name" then "name"
+             when "-name" then "name: desc"
+             when "created" then "created"
+             else "created"
+             end
 
-    page = params[:page] || {}
-    page[:number] = page[:number] && page[:number].to_i > 0 ? page[:number].to_i : 1
-    page[:size] = page[:size] && (1..1000).include?(page[:size].to_i) ? page[:size].to_i : 25
-    total = collection.count
-    providers = get_providers collection
-    
-    variable = case params[:sort]
-      when "created" then "created"
-      else "name"
+      params[:query] ||= "*"
+      response = Client.query(params[:query], from: from, size: size, sort: sort)
+      total = response.total
+      total_pages = (total.to_f / size).ceil
+
+      years = facet_by_year(response.response.aggregations.years.buckets)
+      providers = facet_by_provider(response.response.aggregations.providers.buckets)
     end
 
-    ordered = case params[:sort]
-        when "-name" then collection.sort_by { |hsh| hsh[variable] }.reverse
-        when "-created" then collection.sort_by { |hsh| hsh[variable] }.reverse
-        else collection.sort_by { |hsh| hsh[variable] }
-    end
+    @clients = Kaminari.paginate_array(response.results, total_count: total).page(page).per(size)
 
-
-    # https://github.com/elastic/elasticsearch-rails/issues/338
-    @clients = collection.all unless collection.respond_to?(:each)
-    @clients = Kaminari.paginate_array(ordered, total_count: total).page(page[:number]).per(page[:size])
-
-    meta = { total: total,
-             total_pages: @clients.total_pages,
-             page: page[:number].to_i,
-             providers: providers,
-             years: years }
+    meta = {
+      total: total,
+      total_pages: total_pages,
+      page: page,
+      years: years
+    }
 
     render jsonapi: @clients, meta: meta, include: @include, each_serializer: DataCenterSerializer
   end
