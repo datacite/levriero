ab124b2dcd5229db237853b853ca525388ea0b2d
Martin Fenner <mfenner@datacite.org>
2018-02-19 12:18:52 +0100

    import clients and providers on startup

---
+++ spec/concerns/importable_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "Importable", type: :model, vcr: true do
+  describe "import_from_api" do
+    # return number of clients or providers imported into Elasticsearch
+    it "client" do
+      expect(Client.import_from_api).to eq(1436)
+    end
+
+    it "provider" do
+      expect(Provider.import_from_api).to eq(67)
+    end
+  end
+end
