84a8637632915b1e95c3e6c14601a9098087dff8
Martin Fenner <mfenner@datacite.org>
2018-02-20 00:37:37 +0100

    removed obnsolete variable

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -4,10 +4,6 @@ class ClientSerializer < ActiveModel::Serializer
 
   attributes :name, :symbol, :year, :contact_name, :contact_email, :provider_id, :domains, :url, :is_active, :created, :updated
 
-  def provider_id
-    object.provider_symbol
-  end
-
   def created
     object.created.strftime("%FT%TZ")
   end
