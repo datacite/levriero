80ef7149f0334cbfe0397013eb573005ecede1b0
Martin Fenner <mfenner@datacite.org>
2018-02-24 23:25:08 +0100

    adjust elasticsearch to aws v4 api

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,16 +1,6 @@
-require 'faraday_middleware/aws_signers_v4'
+require 'faraday_middleware/aws_sigv4'
 
-if ENV['AWS_REGION']
-  Elasticsearch::Persistence.client = Elasticsearch::Client.new(host: ENV['ES_HOST'], port: '80', scheme: 'http') do |f|
-    f.request :aws_signers_v4,
-      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-      service_name: 'es',
-      region: ENV['AWS_REGION']
-
-    f.response :logger
-    f.adapter  Faraday.default_adapter
-  end
-else
+if ENV['ES_HOST'] == "elasticsearch:9200"
   config = {
     host: ENV['ES_HOST'],
     transport_options: {
@@ -18,4 +8,14 @@ else
     }
   }
   Elasticsearch::Persistence.client = Elasticsearch::Client.new(host: ENV['ES_HOST'], user: "elastic", password: ENV['ELASTIC_PASSWORD'], log: ENV['LOG_LEVEL'] == "debug")
+else
+  Elasticsearch::Persistence.client = Elasticsearch::Client.new(host: ENV['ES_HOST']) do |f|
+    f.request :aws_sigv4,
+      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+      service: 'es',
+      region: ENV['AWS_REGION']
+
+    f.response :logger
+    f.adapter  Faraday.default_adapter
+  end
 end
