0f5bf38aa1df23b57ace1b2321e4a1ef9b8dec9c
Martin Fenner <martin.fenner@datacite.org>
2018-08-13 23:02:12 +0200

    comment out import workers

--- app/workers/event_delete_worker.rb
+++ app/workers/event_delete_worker.rb
@@ -1,12 +1,12 @@
-class EventDeleteWorker
-  include Shoryuken::Worker
+# class EventDeleteWorker
+#   include Shoryuken::Worker
 
-  shoryuken_options queue: ->{ "#{ENV['RAILS_ENV']}_event" }
+#   shoryuken_options queue: ->{ "#{ENV['RAILS_ENV']}_event" }
 
-  def perform(data)
-    klass = Kernel.const_get(data.fetch("type").chomp('s').capitalize)
-    record = klass.find_by_id(data.fetch("id"))
-    record.delete_record if record.present?
-    record
-  end
-end
+#   def perform(data)
+#     klass = Kernel.const_get(data.fetch("type").chomp('s').capitalize)
+#     record = klass.find_by_id(data.fetch("id"))
+#     record.delete_record if record.present?
+#     record
+#   end
+# end
--- app/workers/event_import_worker.rb
+++ app/workers/event_import_worker.rb
@@ -1,10 +1,10 @@
-class EventImportWorker
-  include Shoryuken::Worker
+# class EventImportWorker
+#   include Shoryuken::Worker
 
-  shoryuken_options queue: ->{ "#{ENV['RAILS_ENV']}_event" }
+#   shoryuken_options queue: ->{ "#{ENV['RAILS_ENV']}_event" }
 
-  def perform(data)
-    klass = Kernel.const_get(data.fetch("type").chomp('s').capitalize)
-    klass.import_record(data)
-  end
-end
+#   def perform(data)
+#     klass = Kernel.const_get(data.fetch("type").chomp('s').capitalize)
+#     klass.import_record(data)
+#   end
+# end
