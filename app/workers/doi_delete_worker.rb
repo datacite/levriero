067de036054a7cbda0961d8c2c31e8bc513a7def
Martin Fenner <martin.fenner@datacite.org>
2018-08-17 10:55:56 +0200

    run more SQS workers in parallel

--- app/workers/doi_delete_worker.rb
+++ app/workers/doi_delete_worker.rb
@@ -1,7 +1,7 @@
 # class EventDeleteWorker
 #   include Shoryuken::Worker
 
-#   shoryuken_options queue: ->{ "#{ENV['RAILS_ENV']}_event" }
+#   shoryuken_options queue: ->{ "#{ENV['RAILS_ENV']}_doi" }
 
 #   def perform(data)
 #     klass = Kernel.const_get(data.fetch("type").chomp('s').capitalize)
