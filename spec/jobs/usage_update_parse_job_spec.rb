583338316bf5bce9ca16170d873bd85caf320c83
Kristian Garza <kj.garza@gmail.com>
2018-09-20 09:10:57 +0200

    tests

---
+++ spec/jobs/usage_update_parse_job_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe UsageUpdateParseJob, type: :job, vcr: true do
+  include ActiveJob::TestHelper
+
+  context "Client" do
+    let(:item) { "https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad" }
+    subject(:job) { UsageUpdateParseJob.perform_later(item) }
+
+    it 'queues the job' do
+      expect { job }.to have_enqueued_job(UsageUpdateParseJob)
+        .on_queue("test_levriero")
+    end
+
+    it 'execute further call' do
+      perform_enqueued_jobs do
+        UsageUpdateParseJob.new.perform(item)
+      end
+    end
+  end
+end
+
+
+
--- spec/models/usage_update_spec.rb
+++ spec/models/usage_update_spec.rb
@@ -35,13 +35,32 @@ describe UsageUpdate, type: :model, vcr: true do
     # end
   end
 
+  describe "queue" do 
+    context "get_total" do
+      it "when is working with AWS" do
+        expect(subject.get_total()).to respond_to(:+)
+        expect(subject.get_total()).not_to respond_to(:each)
+      end
+    end
+
+    context "get_message" do
+      it "should return one message when there are multiple messages" do
+        expect(subject.get_query_url).to respond_to(:each)
+      end
+
+      it "should return no meessage when the queue is empty" do
+        expect(subject.get_query_url).not_to respond_to(:+)
+      end
+    end
+  end
+
   describe "parse_data" do
     context "when the usage event was NOT found" do
       it "should return errors" do
         body = File.read(fixture_path + 'usage_update_nil.json')
         result = OpenStruct.new(body:  JSON.parse(body) )
-        expect(subject.parse_data(result)).to be_a(Array)
-        expect(subject.parse_data(result)).to eq([{"status"=>"404", "title"=>"The resource you are looking for doesn't exist."}])
+        expect(UsageUpdate.parse_data(result)).to be_a(Array)
+        expect(UsageUpdate.parse_data(result)).to eq([{"status"=>"404", "title"=>"The resource you are looking for doesn't exist."}])
       end
     end
 
@@ -57,8 +76,8 @@ describe UsageUpdate, type: :model, vcr: true do
       it "should return errors" do
         body = File.read(fixture_path + 'usage_update_nil.json')
         result = OpenStruct.new(body:  JSON.parse(body) )
-        expect(subject.parse_data(result)).to be_a(Array)
-        expect(subject.parse_data(result)).to eq([{"status"=>"404", "title"=>"The resource you are looking for doesn't exist."}])
+        expect(UsageUpdate.parse_data(result)).to be_a(Array)
+        expect(UsageUpdate.parse_data(result)).to eq([{"status"=>"404", "title"=>"The resource you are looking for doesn't exist."}])
       end
     end
 
@@ -66,7 +85,7 @@ describe UsageUpdate, type: :model, vcr: true do
       it "should parsed it correctly" do
         body = File.read(fixture_path + 'usage_update.json')
         result = OpenStruct.new(body: JSON.parse(body), url:"https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad"  )
-        response = subject.parse_data(result)
+        response = UsageUpdate.parse_data(result)
         expect(response.length).to eq(2)
         expect(response.last.except("uuid")).to eq("subj"=>{"pid"=>"https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad", "issued"=>"2128-04-09"},"total"=>3,"message-action" => "create", "subj-id"=>"https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad", "obj-id"=>"https://doi.org/10.7291/d1q94r", "relation-type-id"=>"unique-dataset-investigations-regular", "source-id"=>"datacite-usage", "occurred-at"=>"2128-04-09", "license" => "https://creativecommons.org/publicdomain/zero/1.0/", "source-token" => ENV['SASHIMI_SOURCE_TOKEN'])
       end
@@ -74,7 +93,7 @@ describe UsageUpdate, type: :model, vcr: true do
       it "should parsed it correctly when it has five metrics  and two DOIs" do
         body = File.read(fixture_path + 'usage_update_3.json')
         result = OpenStruct.new(body: JSON.parse(body), url:"https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad"  )
-        response = subject.parse_data(result)
+        response = UsageUpdate.parse_data(result)
         expect(response.length).to eq(5)
         expect(response.last.except("uuid")).to eq("message-action"=>"create", "subj-id"=>"https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad", "subj"=>{"pid"=>"https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad", "issued"=>"2128-04-09"}, "total"=>208, "obj-id"=>"https://doi.org/10.6071/z7wc73", "relation-type-id"=>"Unique-Dataset-Requests-Machine", "source-id"=>"datacite-usage", "source-token"=>ENV['SASHIMI_SOURCE_TOKEN'], "occurred-at"=>"2128-04-09", "license"=>"https://creativecommons.org/publicdomain/zero/1.0/")
       end
@@ -82,7 +101,7 @@ describe UsageUpdate, type: :model, vcr: true do
       it "should parsed it correctly when it has two metrics per DOI " do
         body = File.read(fixture_path + 'usage_update_2.json')
         result = OpenStruct.new(body: JSON.parse(body), url:"https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad")
-        response = subject.parse_data(result)
+        response = UsageUpdate.parse_data(result)
         expect(response.length).to eq(4)
         expect(response.last.except("uuid")).to eq("message-action"=>"create", "subj-id"=>"https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad", "subj"=>{"pid"=>"https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad", "issued"=>"2128-04-09"}, "total"=>208, "obj-id"=>"https://doi.org/10.6071/z7wc73", "relation-type-id"=>"Unique-Dataset-Requests-Machine", "source-id"=>"datacite-usage", "source-token"=>ENV['SASHIMI_SOURCE_TOKEN'], "occurred-at"=>"2128-04-09", "license"=>"https://creativecommons.org/publicdomain/zero/1.0/")
       end
@@ -90,9 +109,9 @@ describe UsageUpdate, type: :model, vcr: true do
       it "should send a warning if there are more than 4 metrics" do
         body = File.read(fixture_path + 'usage_update_1.json')
         result = OpenStruct.new(body: JSON.parse(body), url:"https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad"  )
-        response = subject.parse_data(result)
+        response = UsageUpdate.parse_data(result)
         expect(response.length).to eq(1)
-        expect(subject.parse_data(result)).to be_a(Array)
+        expect(UsageUpdate.parse_data(result)).to be_a(Array)
         expect(response.last.body).to eq({"errors"=>"There are too many instances in 10.7291/D1Q94R for report https://api.test.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad. There can only be 4"})
       end
     end
@@ -102,22 +121,22 @@ describe UsageUpdate, type: :model, vcr: true do
     let!(:events) {create_list(:event,10)}
     it "should report if there are no works returned by the Queue" do
       result = []
-      expect { subject.push_data(result) }.to output("No works found in the Queue.\n").to_stdout
+      expect { UsageUpdate.push_data(result) }.to output("No works found in the Queue.\n").to_stdout
     end
 
     it "should fail if format of the event is wrong" do
       body = File.read(fixture_path + 'usage_events.json')
       expect = File.read(fixture_path + 'event_data_resp_2')
-      result = JSON.parse(body)
+      # result = JSON.parse(body)
       options = { }
-      expect(subject.push_data(result, options)).to eq(4)
+      expect(UsageUpdate.push_data(body, options)).to eq(4)
     end
 
     it "should fail if format of the event is empty" do
       body = File.read(fixture_path + 'events_empty.json')
-      result = JSON.parse(body)
+      # result = JSON.parse(body)
       options = { }
-      expect(subject.push_data(result, options)).to eq(2)
+      expect((UsageUpdate.push_data(body, options))).to eq(2)
     end
 
     # it "should work with DataCite Event Data 2" do
@@ -129,9 +148,9 @@ describe UsageUpdate, type: :model, vcr: true do
 
     it "should work with a single item" do
       body = File.read(fixture_path + 'usage_events.json')
-      result = JSON.parse(body)
+      result = JSON.parse(body).first.to_json
       options = { }
-      expect(subject.push_item(result.first, options)).to eq(1)
+      expect(UsageUpdate.push_item(result, options)).to eq(true)
     end
   end
 
