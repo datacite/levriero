cd145f59480500303ab51a5e8c5b9eb3e78ffd07
Kristian Garza <kj.garza@gmail.com>
2018-09-20 13:10:18 +0200

    added logging

--- app/jobs/usage_update_import_job.rb
+++ app/jobs/usage_update_import_job.rb
@@ -2,6 +2,16 @@ class UsageUpdateImportJob < ActiveJob::Base
   queue_as :levriero
 
   def perform(item)
-    UsageUpdate.push_item(item)
+    logger = Logger.new(STDOUT)
+    logger.info item
+    response = UsageUpdate.push_item(item)
+    if response.status == 201 
+      logger.info "#{item['subj-id']} #{item['relation-type-id']} #{item['obj-id']} pushed to Event Data service."
+    elsif response.status == 200
+      logger.info "#{item['subj-id']} #{item['relation-type-id']} #{item['obj-id']} pushed to Event Data service for update."
+    elsif response.body["errors"].present?
+      logger.info "#{item['subj-id']} #{item['relation-type-id']} #{item['obj-id']} had an error:"
+      logger.info "#{response.body['errors'].first['title']}"
+    end
   end
 end
