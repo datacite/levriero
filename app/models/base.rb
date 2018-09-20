144cd48911466e9cca8e546586acf68490220465
Kristian Garza <kj.garza@gmail.com>
2018-09-20 09:10:14 +0200

    refactor delete message

--- app/models/base.rb
+++ app/models/base.rb
@@ -9,46 +9,26 @@ class Base
   ICON_URL = "https://raw.githubusercontent.com/datacite/toccatore/master/lib/toccatore/images/toccatore.png"
 
   def queue options={}
-    puts "Queue name has not been specified" unless ENV['ENVIRONMENT'].present?
-    puts "AWS_REGION has not been specified" unless ENV['AWS_REGION'].present?
+    Rails.logger.info "Queue name has not been specified" unless ENV['ENVIRONMENT'].present?
+    Rails.logger.info "AWS_REGION has not been specified" unless ENV['AWS_REGION'].present?
     region = ENV['AWS_REGION'] ||= 'eu-west-1'
     Aws::SQS::Client.new(region: region.to_s, stub_responses: false)
   end
-
-  # def get_total options={}
-  #   req = @sqs.get_queue_attributes(
-  #     {
-  #       queue_url: queue_url, attribute_names: 
-  #         [
-  #           'ApproximateNumberOfMessages', 
-  #           'ApproximateNumberOfMessagesNotVisible'
-  #         ]
-  #     }
-  #   )
-
-  #   msgs_available = req.attributes['ApproximateNumberOfMessages']
-  #   msgs_in_flight = req.attributes['ApproximateNumberOfMessagesNotVisible']
-  #   msgs_available.to_i
-  # end
 
   def get_message options={}
     @sqs.receive_message(queue_url: queue_url, max_number_of_messages: 1, wait_time_seconds: 1)
   end
 
-  def delete_message options={}
-    return 1 if options.messages.size < 1
+  def delete_message message
     reponse = @sqs.delete_message({
       queue_url: queue_url,
-      receipt_handle: options.messages[0][:receipt_handle]    
+      receipt_handle: message[:receipt_handle]    
     })
     if reponse.successful?
-      puts "Message #{options.messages[0][:receipt_handle]} deleted"
-      0
+      Rails.logger.info "Message #{message[:receipt_handle]} deleted"
     else
-      puts "Could NOT delete Message #{options.messages[0][:receipt_handle]}"
-      1
+      Rails.logger.info "Could NOT delete Message #{message[:receipt_handle]}"
     end
-
   end
 
   def queue_url options={}
