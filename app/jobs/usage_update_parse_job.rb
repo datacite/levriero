b2526c18812ce669df3731d6cfffc85a73e39f68
Kristian Garza <kj.garza@gmail.com>
2018-09-24 13:58:37 +0200

    adding meta for subj

--- app/jobs/usage_update_parse_job.rb
+++ app/jobs/usage_update_parse_job.rb
@@ -8,6 +8,12 @@ class UsageUpdateParseJob < ActiveJob::Base
       data = UsageUpdate.parse_data(response, options)
       message  = data.respond_to?("each") ? "[Usage Report Parsing] Successfully parsed Report #{item}"  : "[Usage Report Parsing] Error parsing Report #{item}"
       logger.info message
+      
+      options.merge(
+        report_meta:{
+          report_id: item, 
+          created_by: response.body.dig("data","report","report-header","created-by"), 
+          reporting_period:response.body.dig("data","report","report-header","reporting-period")})
       UsageUpdate.push_data(data, options)
     end
   end
