309b5acc82c385bb489145fe09713bc6ce25a354
Martin Fenner <martin.fenner@datacite.org>
2018-10-07 12:52:17 +0200

    set permissions to trigger crossref agent. #34

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -2,6 +2,7 @@ class AgentsController < ApplicationController
   before_action :authenticate_user_from_token!
 
   def crossref
+    authorize! :import, Crossref
     total = Crossref.import
 
     render json: { message: "Queued import for #{total} DOIs." }.to_json, status: :ok
