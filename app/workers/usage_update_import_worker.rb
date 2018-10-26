4ae5049a754930aa6bd67a70676cd179568a6490
Kristian Garza <kj.garza@gmail.com>
2018-10-26 11:21:14 +0200

    pull from the usage queue

---
+++ app/workers/usage_update_import_worker.rb
@@ -0,0 +1,9 @@
+class UsageUpdateImportWorker
+  include Shoryuken::Worker
+
+  shoryuken_options queue: ->{ "#{ENV['RAILS_ENV']}_usage" }, auto_delete: true
+
+  def perform(sqs_msg, data)
+    UsageUpdate.parse_record(sqs_msg: sqs_msg, data: JSON.parse(data))
+  end
+end
