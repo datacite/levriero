7d4d1a2248b78bc2d70b1ac07e03bebe23109314
Kristian Garza <kj.garza@gmail.com>
2018-11-20 04:45:59 +0100

    moved methods

--- app/models/usage_update.rb
+++ app/models/usage_update.rb
@@ -47,69 +47,6 @@ class UsageUpdate < Base
   end
 
 
-  def self.parse_data report, options={}
-
-    return report.body.fetch("errors") if report.body.fetch("errors", nil).present?
-    return [{ "errors" => { "title" => "The report is blank" }}] if report.body.blank?
-
-    data = report.body.fetch("data", {})
-
-    items = data.fetch("report",{}).key?("gzip") ? self.parse_report(data.dig("report","gzip"),data.dig("report","checksum")) : data.dig("report","report-datasets")
-
-    # items = data.dig("report","report-datasets")
-    header = data.dig("report","report-header")
-    report_id = report.url
-
-    Array.wrap(items).reduce([]) do |x, item|
-      data = { 
-        doi: item.dig("dataset-id").first.dig("value"), 
-        id: normalize_doi(item.dig("dataset-id").first.dig("value")),
-        created: header.fetch("created"), 
-        report_id: report.url,
-        created_at: header.fetch("created")
-      }
-      instances = item.dig("performance", 0, "instance")
-
-      return x += [OpenStruct.new(body: { "errors" => "There are too many instances in #{data[:doi]} for report #{report_id}. There can only be 4" })] if instances.size > 8
-   
-      x += Array.wrap(instances).reduce([]) do |ssum, instance|
-        data[:count] = instance.dig("count")
-        event_type = "#{instance.dig("metric-type")}-#{instance.dig("access-method")}"
-        ssum << format_event(event_type, data, options)
-        ssum
-      end
-    end    
-  end
-
-  def self.decompress_report gzip
-    ActiveSupport::Gzip.decompress(gzip)
-  end
-
-  def self.decode_report encoded_report
-    gzip = Base64.decode64(encoded_report)
-    self.decompress_report(gzip)
-  end
-
-  def self.parse_report encoded_report, checksum
-    fail "Report checksum doesn't match" if self.correct_checksum?(encoded_report, checksum).nil?
-    json = self.decode_report(encoded_report)
-    starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    parser = Yajl::Parser.new
-    pp= parser.parse(json)
-    ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    elapsed = ending - starting
-    puts elapsed # => 9.183449000120163 seconds
-    pp.fetch("report-datasets",[])
-  end
-
-  def self.correct_checksum? encoded_report, checksum
-    # Digest::SHA256.hexdigest(self.decode_report(report))
-    puts checksum
-    puts Digest::SHA256.hexdigest(Base64.decode64(encoded_report))
-    return nil if Digest::SHA256.hexdigest(Base64.decode64(encoded_report)) != checksum
-    true
-  end
-
   def self.format_event type, data, options={}
     fail "Not type given. Report #{data[:report_id]} not proccessed" if type.blank?
     fail "Report_id is missing" if data[:report_id].blank?
