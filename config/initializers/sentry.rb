2ff112eeb3b631d7e8dc668ac75fa8cbdc86e6ea
Martin Fenner <martin.fenner@datacite.org>
2019-03-12 12:25:45 +0100

    include application name in sentry release tag

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Raven.configure do |config|
   config.dsn = ENV["SENTRY_DSN"]
-  config.release = Levriero::Application::VERSION
+  config.release = "levriero:" + Levriero::Application::VERSION
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
