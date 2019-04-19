26ff9d694fde07bffe3d5596b94ab0ddda990beb
Martin Fenner <martin.fenner@datacite.org>
2019-04-19 10:46:55 +0200

    don't log git commands

--- config/initializers/_version.rb
+++ config/initializers/_version.rb
@@ -1,6 +1,6 @@
 module Levriero
   class Application
-    g = Git.open(Rails.root, :log => Logger.new(STDOUT))
+    g = Git.open(Rails.root)
     VERSION = g.tags.map { |t| Gem::Version.new(t.name) }.sort.last.to_s
     REVISION = g.object('HEAD').sha
   end
