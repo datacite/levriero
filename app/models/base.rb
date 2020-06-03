9d4d2193361e6877e9c7044a62ee691666f2eee2
kjgarza <kgarza@datacite.org>
2020-06-03 15:56:56 +0200

    refactor: config for reports and deal with handle

--- app/models/base.rb
+++ app/models/base.rb
@@ -154,7 +154,7 @@ class Base
   end
 
   def self.doi_from_url(url)
-    if /\A(?:(http|https):\/\/(dx\.)?(doi.org|handle.test.datacite.org)\/)?(doi:)?(10\.\d{4,5}\/.+)\z/.match(url)
+    if /\A(?:(http|https):\/\/(dx\.)?(doi.org|handle.test.datacite.org|handle.stage.datacite.org)\/)?(doi:)?(10\.\d{4,5}\/.+)\z/.match(url)
       uri = Addressable::URI.parse(url)
       uri.path.gsub(/^\//, '').downcase
     end
