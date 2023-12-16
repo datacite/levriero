8712982c0ce326a57471c8de0ac498ef4686b95c
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2023-12-16 10:32:18 +0200

    rework crossref import jsob

--- app/models/crossref.rb
+++ app/models/crossref.rb
@@ -17,12 +17,18 @@ class Crossref < Base
     until_date = options[:until_date].present? ? Date.parse(options[:until_date]) : Date.current
 
     crossref = Crossref.new
-    crossref.queue_jobs(crossref.unfreeze(from_date: from_date.strftime("%F"),
-                                          until_date: until_date.strftime("%F"), host: true))
+
+    crossref.queue_jobs(crossref.unfreeze(
+      from_date: from_date.strftime("%F"),
+      until_date: until_date.strftime("%F"), host: true))
   end
 
   def source_id
     "crossref"
+  end
+
+  def allowed_relationship_types
+    ["cites", "references", "is-supplemented-by"]
   end
 
   def get_query_url(options = {})
@@ -48,74 +54,56 @@ class Crossref < Base
   def queue_jobs(options = {})
     options[:offset] = options[:offset].to_i || 0
     options[:rows] = options[:rows].presence || job_batch_size
-    options[:from_date] =
-      options[:from_date].presence || (Time.now.to_date - 1.day).iso8601
-    options[:until_date] =
-      options[:until_date].presence || Time.now.to_date.iso8601
+    options[:from_date] = options[:from_date].presence || (Time.now.to_date - 1.day).iso8601
+    options[:until_date] = options[:until_date].presence || Time.now.to_date.iso8601
     options[:content_type] = "json"
 
-    total, cursor = get_total(options)
+    count, cursor = process_data(options)
+    total = count
 
-    if total.positive?
-      # walk through results paginated via cursor
-      total_pages = (total.to_f / job_batch_size).ceil
-      error_total = 0
-
-      (0...total_pages).each do |page|
-        options[:offset] = page * job_batch_size
-        options[:total] = total
-        options[:cursor] = cursor
-        count, cursor = process_data(options)
-      end
-      text = "Queued import for #{total} DOIs updated #{options[:from_date]} - #{options[:until_date]}."
-    else
+    if count.zero?
       text = "No DOIs updated #{options[:from_date]} - #{options[:until_date]}."
+    else
+      loop do
+        count, cursor = process_data(options)
+
+        break if count.zero? || cursor.blank?
+
+        options[:cursor] = cursor
+        total += count
+      end
+
+      text = "Queued import for #{total} DOIs updated #{options[:from_date]} - #{options[:until_date]}."
     end
 
-    Rails.logger.info "[Event Data] #{text}"
+    Rails.logger.info("[Event Data] #{text}")
+    send_slack_notification(options, text)
 
-    # send slack notification
-    options[:level] = if total.zero?
-                        "warning"
-                      elsif error_total.positive?
-                        "danger"
-                      else
-                        "good"
-                      end
-    options[:title] = "Report for #{source_id}"
-    if options[:slack_webhook_url].present?
-      send_notification_to_slack(text,
-                                 options)
-    end
-
-    # return number of works queued
     total
   end
 
   def push_data(result, _options = {})
-    return result.body.fetch("errors") if result.body.fetch("errors", nil).present?
-
-    items = result.body.dig("data", "message", "relationships")
-
-    Array.wrap(items).map do |item|
-      CrossrefImportJob.perform_later(item)
+    if result.body.fetch("errors", nil).present? do
+      return result.body.fetch("errors")
     end
+
+    items = Array.wrap(result
+      .body
+      .dig("data", "message", "relationships")
+      .select { |item| allowed_relationship_types.includes?(item["relationship_type"].to_s.dasherize) })
+      .map { |item| CrossrefImportJob.perform_later(item) }
 
     [items.length, result.body.dig("data", "message", "next-cursor")]
   end
 
   def self.push_item(item)
+    return if ENV["STAFF_ADMIN_TOKEN"].blank?
+
     uuid = SecureRandom.uuid
     subj_id = item.fetch("subject", "id")
     obj_id = item.fetch("object", "id")
-
     subj = cached_crossref_response(subj_id)
     obj = cached_datacite_response(obj_id)
-
-    return if ENV["STAFF_ADMIN_TOKEN"].blank?
-
-    # so the id is important!!!
-    # need to check this out
     push_url = ENV["LAGOTTINO_URL"] + "/events/#{uuid}"
 
     data = {
@@ -127,7 +115,7 @@ class Crossref < Base
           "subjId" => subj_id,
           "objId" => obj_id,
           "relationTypeId" => item["relationship_type"].to_s.dasherize,
-          "sourceId" => "crossref",
+          "sourceId" => source_id,
           "sourceToken" => item["source_token"], # well this may very well be null now. it might be used to do a lookup on the crossref side.
           "occurredAt" => item["updated_time"],
           "timestamp" => Time.now.utc,
@@ -152,6 +140,17 @@ class Crossref < Base
     elsif response.body["errors"].present?
       Rails.logger.error "[Event Data] #{item['subj_id']} #{item['relation_type_id']} #{item['obj_id']} had an error: #{response.body['errors']}"
       Rails.logger.error data.inspect
+    end
+  end
+
+  private
+
+  def send_slack_notification(options, text)
+    options[:level] = total.zero? ? "warning" : "good"
+    options[:title] = "Report for #{source_id}"
+
+    if options[:slack_webhook_url].present?
+      send_notification_to_slack(text, options)
     end
   end
 end
