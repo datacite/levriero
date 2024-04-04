39060e5909a6f631eba95da809282002932cd7e7
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-04-04 16:47:51 +0200

    remove testing endpoint

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,10 +2,4 @@ class IndexController < ApplicationController
   def index
     render plain: ENV["SITE_TITLE"]
   end
-
-  def show
-    id = params[:id]
-    foo = Rails.cache.fetch("wendel/#{id}", expires_in: 1.day) { "cached_wendel_#{id}_test" }
-    render json: {message: foo}
-  end
 end
