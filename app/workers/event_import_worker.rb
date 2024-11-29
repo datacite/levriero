a06893ac5fb1db089a0c4a3cadb1b4d15a369ba7
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-11-29 14:41:26 +0200

    add event import worker specs

--- app/workers/event_import_worker.rb
+++ app/workers/event_import_worker.rb
@@ -4,6 +4,43 @@ class EventImportWorker
   shoryuken_options queue: -> { "#{ENV['RAILS_ENV']}_events" }, auto_delete: true
 
   def perform(sqs_msg=nil, data=nil)
-    Event.process_message(sqs_msg, data)
+    if data.blank?
+      Rails.logger.info("[EventImportWorker] data object is blank.")
+      return
+    end
+
+    response = post_to_event_service(data)
+    prefix = log_prefix(data)
+    handle_logging(data, response, prefix)
+  end
+
+  private
+
+  def post_to_event_service(data)
+    Maremma.post(
+      "#{ENV["LAGOTTINO_URL"]}/events",
+      data: data.to_json,
+      bearer: ENV["STAFF_ADMIN_TOKEN"],
+      content_type: "application/vnd.api+json",
+      accept: "application/vnd.api+json; version=2")
+  end
+
+  def log_prefix(data)
+    subj_id = data["data"]["attributes"]["subjId"]
+    relation_type_id = data["data"]["attributes"]["relationTypeId"]
+    obj_id = data["data"]["attributes"]["objId"]
+
+    "[EventImportWorker] #{subj_id} #{relation_type_id} #{obj_id}"
+  end
+
+  def handle_logging(data, response, prefix)
+    if response.status == 200 || response.status == 201
+      Rails.logger.info("#{prefix} pushed to the Event Data service.")
+    elsif response.status == 409
+      Rails.logger.info("#{prefix} already pushed to the Event Data service.")
+    elsif response.body["errors"].present?
+      Rails.logger.error("#{prefix} had an error: #{response.body["errors"]}")
+      Rails.logger.error(data.inspect)
+    end
   end
 end
