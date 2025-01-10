3a4093298ea387c7feaf7489e0157e3e0e9dcf9a
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-01-10 09:54:01 +0200

    add logging for debugging

--- app/workers/doi_import_worker.rb
+++ app/workers/doi_import_worker.rb
@@ -4,6 +4,8 @@ class DoiImportWorker
   shoryuken_options queue: -> { "#{ENV['RAILS_ENV']}_doi" }, auto_delete: true
 
   def perform(sqs_msg, data)
+    Rails.logger.info("Start of doi import worker")
+    Rails.logger.info(data)
     Doi.parse_record(sqs_msg: sqs_msg, data: JSON.parse(data))
   end
 end
