9dcaaa1493acabc1bdef354be8d1aeef6086ddd2
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-01-21 17:44:43 +0200

    remove superfluous logging

--- app/models/base.rb
+++ app/models/base.rb
@@ -252,7 +252,6 @@ class Base
     response = Maremma.get(url)
 
     if response.status != 200
-      Rails.logger.info("[EventImportWorker]: response failed (#{url}):#{response.inspect}")
       Rails.logger.info "DOI #{doi} not found"
       return {}
     end
