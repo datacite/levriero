96c714092d869b720e2c3b9efae67aefa68d99ac
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-01-23 15:57:17 +0200

    modify update usage to use the events queue

--- spec/models/usage_update_spec.rb
+++ spec/models/usage_update_spec.rb
@@ -2,15 +2,6 @@ require "rails_helper"
 
 describe UsageUpdate, type: :model, vcr: true do
   describe "get_data" do
-    # context "when there is ONE message" do
-    #   it "should return the data for one message" do
-    #     options ={}
-    #     message= "https://api.stage.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad"
-    #     response = UsageUpdate.get_data(message,options)
-    #     expect(response.body["data"]["report"]["report-header"]["report-name"]).to eq("Dataset Master Report")
-    #   end
-    # end
-
     context "when there are NO messages" do
       it "should return empty" do
         options = {}
@@ -20,21 +11,6 @@ describe UsageUpdate, type: :model, vcr: true do
       end
     end
   end
-
-  # describe "queue" do
-  #   context "get_total" do
-  #     it "when is working with AWS" do
-  #       expect(subject.get_total()).to respond_to(:+)
-  #       expect(subject.get_total()).not_to respond_to(:each)
-  #     end
-  #   end
-
-  #   context "get_message" do
-  #     it "should return one message when there are multiple messages" do
-  #       expect(subject.get_query_url).to respond_to(:each)
-  #     end
-  #   end
-  # end
 
   describe "parse_data" do
     context "when the usage event was NOT found" do
@@ -48,18 +24,8 @@ describe UsageUpdate, type: :model, vcr: true do
         )
         expect(response).to be_a(Array)
         expect(response).to be_empty
-
-        # expect(response).to eq([{"status"=>"404", "title"=>"The resource you are looking for doesn't exist."}])
       end
     end
-
-    # context "when the queue is empty" do
-    #   it "should return errors" do
-    #     result = OpenStruct.new(body: { "errors" => "Queue is empty" })
-    #     expect(subject.parse_data(result)).to be_a(Array)
-    #     expect(subject.parse_data(result)).to eq([{"status"=>"404", "title"=>"The resource you are looking for doesn't exist."}])
-    #   end
-    # end
 
     context "when the usage report was NOT found" do
       it "should return errors" do
@@ -73,7 +39,6 @@ describe UsageUpdate, type: :model, vcr: true do
 
         expect(response).to be_a(Array)
         expect(response).to be_empty
-        # expect(response).to eq([{"status"=>"404", "title"=>"The resource you are looking for doesn't exist."}])
       end
     end
 
@@ -116,23 +81,10 @@ describe UsageUpdate, type: :model, vcr: true do
         )
       end
 
-      # it "should parsed it correctly from call" do
-      #   result = Maremma.get("https://api.stage.datacite.org/reports/1ca6c11f-dcc8-47ed-be45-53499687f0d1", host: "https://api.stage.datacite.org/")
-      #   report = Report.new(result)
-      #   args = { header: report.header, url: report.report_url }
-      #   response = Report.translate_datasets(result.body.dig("data","report","report-datasets"), args)
-
-      #   expect(response.length).to eq(1642)
-      #   doi_instances =response.select {|instance| instance.dig("obj-id") == "https://doi.org/10.7272/q6qn64nk" }
-      #   total_requests_regular = doi_instances.select {|instance| instance.dig("relation-type-id") == "total-dataset-requests-regular"}
-      #   expect(total_requests_regular.first.dig("total")).to eq(1083)
-      # end
-
       it "should parsed it correctly from dataone with strange doi names" do
         body = File.read("#{fixture_path}dataone.json")
         result = OpenStruct.new(body: JSON.parse(body),
                                 url: "https://api.stage.datacite.org/reports/f0e06846-7af1-4e43-a32b-8d299e99bd21")
-        # response = UsageUpdate.parse_data(result)
         report = Report.new(result)
         args = { header: report.header, url: report.report_url }
         response = Report.translate_datasets(
@@ -140,14 +92,12 @@ describe UsageUpdate, type: :model, vcr: true do
         )
 
         expect(response.last.fetch("obj-id")).to eq("https://doi.org/10.5063/aa/bowdish.122.10")
-        # expect(doi_instances.first.dig("total")).to eq(1083)
       end
 
       it "should parsed it correctly when it has five metrics  and two DOIs" do
         body = File.read("#{fixture_path}usage_update_3.json")
         result = OpenStruct.new(body: JSON.parse(body),
                                 url: "https://api.stage.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad")
-        # response = UsageUpdate.parse_data(result)
         report = Report.new(result)
         args = { header: report.header, url: report.report_url }
         response = Report.translate_datasets(
@@ -161,7 +111,6 @@ describe UsageUpdate, type: :model, vcr: true do
       it "should parsed it correctly when it has two metrics per DOI " do
         body = File.read("#{fixture_path}usage_update_2.json")
         result = OpenStruct.new(body: JSON.parse(body), url: "https://api.stage.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad")
-        # response = UsageUpdate.parse_data(result)
         report = Report.new(result)
         args = { header: report.header, url: report.report_url }
         response = Report.translate_datasets(
@@ -177,7 +126,6 @@ describe UsageUpdate, type: :model, vcr: true do
         body = File.read("#{fixture_path}usage_update_1.json")
         result = OpenStruct.new(body: JSON.parse(body),
                                 url: "https://api.stage.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad")
-        # response = UsageUpdate.parse_data(result)
         report = Report.new(result)
         args = { header: report.header, url: report.report_url }
         response = Report.translate_datasets(
@@ -196,7 +144,6 @@ describe UsageUpdate, type: :model, vcr: true do
           body = File.read("#{fixture_path}datacite_resolution_report_2018-09_encoded.json")
           result = OpenStruct.new(body: JSON.parse(body),
                                   url: "https://api.stage.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad")
-          # parsed = UsageUpdate.parse_data(result)
           expect(Report.new(result).compressed_report?).to be(true)
         end
       end
@@ -209,48 +156,24 @@ describe UsageUpdate, type: :model, vcr: true do
         end
       end
     end
-
-    # describe "parse_data compressed 2" do
-    #   context "when the usage event is ok" do
-    #     it "should return report parsed" do
-    #       body = File.read(fixture_path + 'datacite_resolution_report_2018-09_encoded.json')
-    #       result = OpenStruct.new(body:  JSON.parse(body), url:"https://api.stage.datacite.org/reports/5cac6ca0-9391-4e1d-95cf-ba2f475cbfad" )
-    #       parsed = Report.new(result).parse_data
-    #       # puts parsed
-    # checksum is wrong
-    #       expect(parsed).to be_a(Array)
-    #       expect(parsed.size).to eq(95056)
-    #     end
-    #   end
-    # end
   end
 
-  describe "wrap event" do
-    let(:options) do
-      { report_meta: {
-        report_id: "1a6e79ea-5291-4f5f-a25e-2cb071715bfc",
-        created_by: "datacite",
-        reporting_period: "",
-      } }
-    end
-    let(:item) do
-      { "obj-id" => "https://doi.org/10.14278/rodaretest.11", "total" => 45 }
-    end
+  context "push_item" do
+    it "should place a message on the events queue" do
+      allow(ENV).to(receive(:[]).with("STAFF_ADMIN_TOKEN").and_return("token"))
+      allow(UsageUpdate).to(receive(:send_event_import_message).and_return(nil))
+      allow(Rails.logger).to(receive(:info))
 
-    it "should format correctly" do
-      # expect((UsageUpdate.wrap_event(item,options)).dig("data","attributes","obj")).to eq({"id"=>"https://doi.org/10.14278/rodaretest.11", "type"=>"dataset", "name"=>"Large Image", "author"=>[{"given_name"=>"Tester", "family_name"=>"Test"}], "publisher"=>"Rodare", "date_published"=>"2018-04-10", "date_modified"=>"2018-10-28T02:01:02.000Z", "registrant_id"=>"datacite.tib.hzdr"})
-      # expect((UsageUpdate.wrap_event(item,options)).dig("data","attributes","total")).to eq(45)
-    end
-  end
+      item = {
+        "subj-id": "subj-id",
+        "relation-type-id": "relation-type-id",
+        "obj-id": "obj-id"
+      }.to_json
 
-  context "push_data" do
-    let!(:events) { create_list(:event, 10) }
+       UsageUpdate.push_item(item)
 
-    it "should work with a single item" do
-      body = File.read("#{fixture_path}usage_events.json")
-      result = JSON.parse(body).first.to_json
-      options = {}
-      # expect(UsageUpdate.push_item(result, options)).to eq(true)
+      expect(UsageUpdate).to(have_received(:send_event_import_message).once)
+      expect(Rails.logger).to(have_received(:info).with("[Event Data] subj-id relation-type-id obj-id sent to the events queue."))
     end
   end
 end
