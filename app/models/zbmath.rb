4452feb573279d1bcaefe0e25bb6c7cadf77add1
Mike Bennett <mbennett@datacite.org>
2025-03-28 13:18:15 +0000

    Add fixes for the various uses of arXiv identifiers

--- app/models/zbmath.rb
+++ app/models/zbmath.rb
@@ -3,14 +3,15 @@
 include Bolognese::MetadataUtils
 class Zbmath
   LICENSE = "https://creativecommons.org/publicdomain/zero/1.0/"
+  ARXIV_PREFIX = ENV["ARXIV_PREFIX"] || "10.48550"
 
   include Queueable
 
   include Cacheable
 
   def self.import_by_month(options = {})
-    from_date = (options[:from_date].present? ? Date.parse(options[:from_date]) : Date.current).beginning_of_month
-    until_date = (options[:until_date].present? ? Date.parse(options[:until_date]) : Date.current).end_of_month
+    from_date = (options[:from_date].present? ? DateTime.parse(options[:from_date]) : Date.current).beginning_of_month
+    until_date = (options[:until_date].present? ? DateTime.parse(options[:until_date]) : Date.current).end_of_month
 
     # get first day of every month between from_date and until_date
     (from_date..until_date).select { |d| d.day == 1 }.each do |m|
@@ -22,8 +23,8 @@ class Zbmath
   end
 
   def self.import(options = {})
-    from_date = options[:from_date].present? ? Date.parse(options[:from_date]) : Date.current - 1.day
-    until_date = options[:until_date].present? ? Date.parse(options[:until_date]) : Date.current
+    from_date = options[:from_date].present? ? DateTime.parse(options[:from_date]) : Date.current - 1.day
+    until_date = options[:until_date].present? ? DateTime.parse(options[:until_date]) : Date.current
     Rails.logger.info "Importing ZBMath Records updated from #{from_date.strftime('%F')} until #{until_date.strftime('%F')}."
     zbmath = Zbmath.new
     zbmath.get_records(from: from_date.strftime("%F"), until: until_date.strftime("%F"))
@@ -31,6 +32,29 @@ class Zbmath
 
   def source_id
     "zbmath"
+  end
+
+  def check_for_arxiv(record)
+    # Check if the record has an ARXIV identifier as the primary identifier and convert it to a DOI
+    identifier = record.xpath_first(record._source, ".//metadata/resource/identifier")
+    if identifier && identifier.attributes.fetch("identifierType").value.downcase == "arxiv"
+      puts "ARXIV found in main identifier- converting to DOI"
+      arxiv_identifier = if identifier.text.downcase.start_with?("arxiv:")
+                           "arXiv.#{identifier.text[6..]}"
+                         else
+                           "arXiv.#{identifier.text}"
+                         end
+      "#{ARXIV_PREFIX}/#{arxiv_identifier}"
+    end
+  end
+
+  def remove_extra_identifiers(record)
+    # If the record has more than one <identifier> element, bolognese will error out, so as a workaround we remove all but the first
+    identifiers = record.metadata.get_elements("//resource/identifier")
+    if identifiers.size > 1
+      identifiers[1..].each { |i| record.metadata.children[0].delete_element(i) }
+    end
+    record
   end
 
   def get_records(options = {})
@@ -42,11 +66,16 @@ class Zbmath
       # simpler and more efficient than parsing the XML, restructuring the tree and then serializing back to string).
       #
       # Using the .full.each pattern allows transparent handling of the resumption token pattern in the OAI protocol
-      # rather than having to deal with it manually. The client should only load one page into memory ay a time, so the
+      # rather than having to deal with it manually. The client should only load one page into memory at a time, so the
       # efficiency should be ok.
-      response = client.list_records(metadata_prefix: "datacite_articles", from: options[:from],
-                                     until: options[:until]).full.each do |record|
-        ZbmathImportJob.perform_later(record.metadata.to_s[12..-14])
+      client.list_records(metadata_prefix: "datacite_articles", from: options[:from],
+                          until: options[:until]).full.each do |record|
+        puts "Processing #{record.header.identifier}"
+        # Check if the record has a DOI via arXiv
+        doi = check_for_arxiv(record)
+        # Remove any extra identifiers
+        record = remove_extra_identifiers(record)
+        ZbmathImportJob.perform_later(record.metadata.to_s[10..-12], doi: doi )
         count += 1
       end
       count
@@ -56,24 +85,63 @@ class Zbmath
     end
   end
 
-  def self.parse_zbmath_record(record)
-    meta = read_datacite(string: record)
+  def self.parse_zbmath_record(record, options = {})
 
-    # Check that the record is for a DataCite DOI
+    meta = read_datacite(string: record, doi: options[:doi].present? ? options[:doi] : nil)
+
+    # Extract any arXiv identifiers to potentially fill in for a missing DOI
+    arxiv_identifier = options[:doi]
+
+    if arxiv_identifier.blank?
+      arxiv_identifier = meta.fetch("identifiers", []).detect do |r|
+        r["identifierType"].downcase == "arxiv"
+      end&.fetch("identifier", nil)
+      if arxiv_identifier.present?
+        unless arxiv_identifier.start_with?("arxiv:")
+          arxiv_identifier = "arXiv.#{arxiv_identifier}"
+        end
+        "#{ARXIV_PREFIX}/#{arxiv_identifier}"
+      end
+    end
+
+    if arxiv_identifier.blank?
+      # Check if there's an an arXiv identifier in the "zbMATH identifier" value
+      zbmath_identifier = meta.fetch("identifiers", []).detect do |r|
+        r["identifierType"] == "zbMATH Identifier"
+      end&.fetch("identifier", nil)
+      if zbmath_identifier.present? && zbmath_identifier.downcase.start_with?("arxiv:")
+        puts "ARXIV found in zbMATH identifier - converting to DOI"
+        arxiv_identifier = "#{ARXIV_PREFIX}/arxiv.#{zbmath_identifier[6..]}"
+      end
+    end
+
+    # Check that the record is for a DOI
     doi = meta.fetch("doi", nil)
+
+    if doi.blank? && arxiv_identifier.present?
+      doi = arxiv_identifier
+    end
+
     return nil if doi.blank?
 
+    # Populate information about the DOI for use in events
     ra = cached_doi_ra(doi)
-
     pid = normalize_doi(doi)
 
-    if ra == "DataCite"
-      subj = cached_datacite_response(pid)
-    elsif ra == "Crossref"
-      subj = cached_crossref_response(pid)
-    else
-      subj = {}
-    end
+    # TEMPORARY DISABLE FOR TESTING
+    # subj = if ra == "DataCite"
+    #          cached_datacite_response(pid)
+    #        elsif ra == "Crossref"
+    #          cached_crossref_response(pid)
+    #        else
+    #          {}
+    #        end
+    subj = {}
+
+    # If there's an arXiv identifier as well, populate information for use in adding any
+    arxiv_pid = arxiv_identifier.present? && arxiv_identifier.downcase != doi.downcase ? normalize_doi(arxiv_identifier) : nil
+    # arxiv_subj = arxiv_pid.present? ? cached_datacite_response(arxiv_pid) : {}
+    arxiv_subj = {}
 
     # parse out valid related identifiers
     related_doi_identifiers = Array.wrap(meta.fetch("related_identifiers", nil)).select do |r|
@@ -82,33 +150,35 @@ class Zbmath
 
     # loop through related identifiers and build event objects
     items = Array.wrap(related_doi_identifiers).reduce([]) do |x, item|
-
       related_identifier = item.fetch("relatedIdentifier", nil).to_s.strip.downcase
       related_identifier_type = item.fetch("relatedIdentifierType", nil).to_s.strip
 
+
       if related_identifier_type == "DOI"
         obj_id = normalize_doi(related_identifier)
-
         # Get RA and populate obj
         related_ra = cached_doi_ra(related_identifier)
-        obj = if related_ra == "DataCite"
-                cached_datacite_response(obj_id)
-              elsif related_ra == "Crossref"
-                # Don't bother hitting Crossref API if we won't process the relationship
-                if ra == "DataCite"
-                  cached_crossref_response(obj_id)
-                else
-                  {}
-                end
-              else
-                {}
-              end
+        # obj = if related_ra == "DataCite"
+        #         cached_datacite_response(obj_id)
+        #       elsif related_ra == "Crossref"
+        #         # Don't bother hitting Crossref API if we won't process the relationship
+        #         if ra == "DataCite" || arxiv_pid.present?
+        #           cached_crossref_response(obj_id)
+        #         else
+        #           {}
+        #         end
+        #       else
+        #         {}
+        #       end
+        obj = {}
       else
         # It's a URL so no PID object on the other end
         obj_id = normalize_url(related_identifier)
         obj = {}
         related_ra = nil
       end
+
+      # Add DataCite <-> Other events
       if ra == "DataCite" || related_ra == "DataCite"
         x << { "message_action" => "create",
                "id" => SecureRandom.uuid,
@@ -123,14 +193,29 @@ class Zbmath
                "obj" => obj,
                "source_token" => ENV["ZBMATH_RELATED_SOURCE_TOKEN"] || "4c891c31-519e-4d98-ac8d-876ad0f28635" }
       end
+
+      if arxiv_pid.present?
+        x << { "message_action" => "create",
+               "id" => SecureRandom.uuid,
+               "subj_id" => arxiv_pid,
+               "obj_id" => obj_id,
+               "relation_type_id" => item["relationType"].to_s.underscore,
+               "source_id" => "zbmath_related",
+               "occurred_at" => Time.zone.now.utc,
+               "timestamp" => Time.zone.now.iso8601,
+               "license" => LICENSE,
+               "subj" => arxiv_subj,
+               "obj" => obj,
+               "source_token" => ENV["ZBMATH_RELATED_SOURCE_TOKEN"] || "4c891c31-519e-4d98-ac8d-876ad0f28635" }
+      end
       x
     end
 
-    if ra == "DataCite"
-    # extract creator identifiers
+    if ra == "DataCite" or arxiv_pid.present?
+      # extract creator identifiers
       creator_identifiers = Array.wrap(meta.fetch("creators", nil)).select do |c|
         c["nameIdentifiers"].present? && c["nameIdentifiers"].any? do |n|
-          n["nameIdentifierScheme"] == "zbMATH Author Code"
+          n["nameIdentifierScheme"] == "zbMATH Author Code" && n["nameIdentifier"] != ":unav"
         end
       end
 
@@ -142,14 +227,14 @@ class Zbmath
         name_identifiers.each do |n|
           x << { "message_action" => "create",
                  "id" => SecureRandom.uuid,
-                 "subj_id" => pid,
+                 "subj_id" => ra == "DataCite" ? pid : arxiv_pid,
                  "obj_id" => "https://zbmath.org/authors/#{n['nameIdentifier']}",
                  "relation_type_id" => "is_authored_by",
                  "source_id" => "zbmath_author",
                  "occurred_at" => Time.zone.now.utc,
                  "timestamp" => Time.zone.now.iso8601,
                  "license" => LICENSE,
-                 "subj" => subj,
+                 "subj" => ra == "DataCite" ? subj : arxiv_subj,
                  "obj" => {},
                  "source_token" => ENV["ZBMATH_AUTHOR_SOURCE_TOKEN"] || "759c2591-7161-4c17-8e35-b3e1a28b4568" }
         end
@@ -170,16 +255,17 @@ class Zbmath
                    end
         x << { "message_action" => "create",
                "id" => SecureRandom.uuid,
-               "subj_id" => pid,
+               "subj_id" => ra == "DataCite" ? pid : arxiv_pid,
                "obj_id" => item_url,
                "relation_type_id" => "is_identical_to",
                "source_id" => "zbmath_identifier",
                "occurred_at" => Time.zone.now.utc,
                "timestamp" => Time.zone.now.iso8601,
                "license" => LICENSE,
-               "subj" => subj,
+               "subj" => ra == "DataCite" ? subj : arxiv_subj,
                "obj" => {},
                "source_token" => ENV["ZBMATH_IDENTIFIER_SOURCE_TOKEN"] || "9908552f-593b-4825-817e-bca48644624b" }
+
         x
       end)
     end
