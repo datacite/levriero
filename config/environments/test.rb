000402c9efc790ec0059619352f18b4aba91b546
kudakwashe SIziva <kuda.siziva@datacite.org>
2026-01-26 14:54:58 +0100

    add config level for tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -48,4 +48,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "warn")
 end
