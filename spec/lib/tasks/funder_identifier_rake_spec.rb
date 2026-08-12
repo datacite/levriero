bb9f97e29bf6219338c3414b32108f4aedd17673
Bryceson Laing <bklaing2@gmail.com>
2026-08-12 14:20:38 -0500

    remove linking events for funders

--- spec/lib/tasks/funder_identifier_rake_spec.rb
+++ spec/lib/tasks/funder_identifier_rake_spec.rb
@@ -52,84 +52,23 @@ describe "funder_identifier:import", vcr: true do
 end
 
 describe "push_item" do
-  it "returns nil when the doi is blank" do
-    expect(FunderIdentifier.push_item({ attributes: { doi: nil } })).to(eq(nil))
-  end
+  it "does not send events (out of scope)" do
+    allow(FunderIdentifier).to(receive(:send_event_import_message).and_return(nil))
 
-  describe "when STAFF_ADMIN_TOKEN" do
-    before(:each) do
-      allow(ENV).to(receive(:[]).with("LAGOTTINO_URL").and_return("https://fake.lagattino.com"))
-      allow(ENV).to(receive(:[]).with("DATACITE_FUNDER_SOURCE_TOKEN").and_return("DATACITE_FUNDER_SOURCE_TOKEN"))
-      allow(Base).to(receive(:cached_datacite_response).and_return({ "foo" => "bar" }))
-      allow(Base).to(receive(:cached_funder_response).and_return({ "bar" => "foo" }))
-      allow(Maremma).to(receive(:post).and_return(OpenStruct.new(status: 200)))
-      allow(Time).to(receive_message_chain(:zone, :now, :iso8601).and_return("2023-11-15T12:17:47Z"))
-      allow(FunderIdentifier).to(receive(:send_event_import_message).and_return(nil))
-    end
-
-    describe "is valid" do
-      it "sends to events queue for those funder identifiers with funder identifier type 'Crossref Funder Id'" do
-        item = {
-          "attributes" => {
-            "doi" => "https://doi.org/10.0001/foo.bar",
-            "updated" => "2023-11-15",
-            "fundingReferences" => [
-              {
-                "funderIdentifier" => "https://doi.org/10.0001/example.one",
-                "funderIdentifierType" => "Crossref Funder ID",
-              },
-              {
-                "funderIdentifier" => "https://doi.org/10.0001/example.two",
-                "funderIdentifierType" => "Foo",
-              },
-              {
-                "funderIdentifier" => "https://doi.org/10.0001/example.three",
-                "funderIdentifierType" => "Crossref Funder ID",
-              },
-            ],
+    item = {
+      "attributes" => {
+        "doi" => "https://doi.org/10.0001/foo.bar",
+        "updated" => "2023-11-15",
+        "fundingReferences" => [
+          {
+            "funderIdentifier" => "https://doi.org/10.0001/example.one",
+            "funderIdentifierType" => "Crossref Funder ID",
           },
-        }
+        ],
+      },
+    }
 
-        expect(FunderIdentifier.push_item(item)).to(eq(2))
-        expect(FunderIdentifier).to(have_received(:send_event_import_message).twice)
-      end
-
-      it "passes the expected values to lagottino" do
-        item = {
-          "attributes" => {
-            "doi" => "https://doi.org/10.0001/foo.bar",
-            "updated" => "2023-11-15",
-            "fundingReferences" => [
-              {
-                "funderIdentifier" => "https://doi.org/10.0001/example.one",
-                "funderIdentifierType" => "Crossref Funder ID",
-              },
-            ],
-          },
-        }
-
-        json_data = {
-          "data" => {
-            "type" => "events",
-            "attributes" => {
-              "messageAction" => "create",
-              "subjId" => "https://doi.org/10.0001/foo.bar",
-              "objId" => "https://doi.org/10.0001/example.one",
-              "relationTypeId" => "is-funded-by",
-              "sourceId" => "datacite-funder",
-              "sourceToken" => "DATACITE_FUNDER_SOURCE_TOKEN",
-              "occurredAt" => "2023-11-15",
-              "timestamp" => "2023-11-15T12:17:47Z",
-              "license" => "https://creativecommons.org/publicdomain/zero/1.0/",
-              "subj" => { "foo" => "bar" },
-              "obj" => { "bar" => "foo" },
-            },
-          },
-        }
-
-        expect(FunderIdentifier.push_item(item)).to(eq(1))
-        expect(FunderIdentifier).to(have_received(:send_event_import_message).with(json_data).once)
-      end
-    end
+    expect(FunderIdentifier.push_item(item)).to(eq(0))
+    expect(FunderIdentifier).not_to(have_received(:send_event_import_message))
   end
 end
