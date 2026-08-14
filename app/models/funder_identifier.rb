3bee801a0e2fbb6d89293eda5a9f387be1647d0e
Bryceson Laing <bklaing2@gmail.com>
2026-08-14 11:27:05 -0500

    Revert "remove linking events for funders"
    This reverts commit bb9f97e29bf6219338c3414b32108f4aedd17673.

--- app/models/funder_identifier.rb
+++ app/models/funder_identifier.rb
@@ -53,8 +53,72 @@ class FunderIdentifier < Base
   end
 
   def self.push_item(item)
-    # Do not link events for funders, return a count of zero
-    0
+    attributes = item.fetch("attributes", {})
+    doi = attributes.fetch("doi", nil)
+    return nil if doi.blank?
+
+    pid = normalize_doi(doi)
+    funder_identifiers = Array.wrap(attributes.fetch("fundingReferences",
+                                                     nil)).select do |f|
+      f["funderIdentifierType"] == "Crossref Funder ID"
+    end
+
+    push_items = Array.wrap(funder_identifiers).reduce([]) do |ssum, iitem|
+      funder_identifier = iitem.fetch("funderIdentifier",
+                                      nil).to_s.strip.downcase
+      obj_id = normalize_doi(funder_identifier)
+
+      relation_type_id = "is_funded_by"
+      source_id = "datacite_funder"
+      source_token = ENV["DATACITE_FUNDER_SOURCE_TOKEN"]
+
+      if funder_identifier.present? && obj_id.present?
+        subj = cached_datacite_response(pid)
+        obj = cached_funder_response(obj_id)
+
+        ssum << { "message_action" => "create",
+                  "subj_id" => pid,
+                  "obj_id" => obj_id,
+                  "relation_type_id" => relation_type_id,
+                  "source_id" => source_id,
+                  "source_token" => source_token,
+                  "occurred_at" => attributes.fetch("updated"),
+                  "timestamp" => Time.zone.now.iso8601,
+                  "license" => LICENSE,
+                  "subj" => subj,
+                  "obj" => obj }
+      end
+
+      ssum
+    end
+
+    # there can be one or more funder_identifier per DOI
+    Array.wrap(push_items).each do |iiitem|
+      data = {
+        "data" => {
+          "type" => "events",
+          "attributes" => {
+            "messageAction" => iiitem["message_action"],
+            "subjId" => iiitem["subj_id"],
+            "objId" => iiitem["obj_id"],
+            "relationTypeId" => iiitem["relation_type_id"].to_s.dasherize,
+            "sourceId" => iiitem["source_id"].to_s.dasherize,
+            "sourceToken" => iiitem["source_token"],
+            "occurredAt" => iiitem["occurred_at"],
+            "timestamp" => iiitem["timestamp"],
+            "license" => iiitem["license"],
+            "subj" => iiitem["subj"],
+            "obj" => iiitem["obj"],
+          },
+        },
+      }
+
+      send_event_import_message(data)
+
+      Rails.logger.info "[Event Data] #{iiitem['subj_id']} #{iiitem['relation_type_id']} #{iiitem['obj_id']} sent to the events queue."
+    end
+
+    push_items.length
   end
 
   def self.get_funder_metadata(id)
